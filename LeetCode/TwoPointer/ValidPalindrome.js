a3e9742ddf39d3dd34d3a9093239767a35a59f9a
Kim Miso <kimms9725@naver.com>
2023-10-13 23:32:28 +0900

    2023.10.13 Leet code - two pointers

--- LeetCode/TwoPointer/ValidPalindrome.js
+++ LeetCode/TwoPointer/ValidPalindrome.js
@@ -1,23 +1,62 @@
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isPalindrome = function (s) {
+//   let filtered = s.match(/[a-z0-9]/gi);
+
+//   if (!filtered) return true;
+
+//   if (filtered) {
+//     filtered = filtered.join('').toLowerCase();
+//   }
+
+//   let l = 0;
+//   let r = filtered.length - 1;
+//   while (l < r) {
+//     if (filtered[l] !== filtered[r]) {
+//       return false;
+//     }
+//     l++;
+//     r--;
+//   }
+
+//   return true;
+// };
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  let filtered = s.match(/[a-z0-9]/gi);
+  let l = 0;
+  let r = s.length - 1;
 
-  filtered = filtered ? filtered.join('').toLowerCase() : '';
+  const isAlphanumeric = (c) => {
+    const regExp = /[a-zA-Z0-9]/gi;
+    return regExp.test(c);
+  };
 
-  let start = 0;
-  let end = filtered.length - 1;
+  while (l < r) {
+    if (!isAlphanumeric(s[l])) {
+      l++;
+      continue;
+    }
 
-  while (start < end) {
-    if (filtered.charAt(start) === filtered.charAt(end)) {
-      start++;
-      end--;
-    } else {
+    if (!isAlphanumeric(s[r])) {
+      r--;
+      continue;
+    }
+
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
       return false;
     }
+
+    l++;
+    r--;
   }
 
   return true;
 };
+
+console.log(isPalindrome('race a car'));
