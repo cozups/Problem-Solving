0c414c0d329bd258358bb6724a8d27db19606c89
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-09 17:41:06 +0900

    [Silver IV] Title: 괄호, Time: 96 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+const [n, ...cases] = input;
+const ans = [];
+
+for (let i = 0; i < Number(n); i++) {
+  let s = [];
+  let j = 0;
+  for (j = 0; j < cases[i].length; j++) {
+    let c = cases[i][j];
+    if (c === '(') {
+      s.push(c);
+    } else {
+      if (s.length === 0) {
+        ans.push('NO');
+        break;
+      }
+
+      const p = s.pop();
+      if (p === ')') {
+        ans.push('NO');
+        break;
+      }
+    }
+  }
+
+  if (j === cases[i].length) {
+    if (s.length === 0) {
+      ans.push('YES');
+    } else {
+      ans.push('NO');
+    }
+  }
+}
+
+console.log(ans.join('\n'));
