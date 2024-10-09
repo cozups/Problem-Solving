5b333c80be9af4bdb0ba2f10d14a28edb4a90506
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-09 16:01:02 +0900

    [Silver IV] Title: 스택, Time: 228 ms, Memory: 12248 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+const n = +input.shift();
+
+let stack = [];
+let answer = [];
+for (let i = 0; i < n; i++) {
+  let cmd = null;
+  let num = null;
+  if (input[i] === 'pop') {
+    stack.length > 0 ? answer.push(stack.pop()) : answer.push(-1);
+  } else if (input[i] === 'size') {
+    answer.push(stack.length);
+  } else if (input[i] === 'empty') {
+    stack.length > 0 ? answer.push(0) : answer.push(1);
+  } else if (input[i] === 'top') {
+    stack.length > 0 ? answer.push(stack[stack.length - 1]) : answer.push(-1);
+  } else {
+    // push
+    [cmd, num] = input[i].split(' ');
+    stack.push(+num);
+  }
+}
+
+console.log(answer.join('\n'));
