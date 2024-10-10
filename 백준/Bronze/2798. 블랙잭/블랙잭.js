a51f2a339322931efb44dce03a2e3a3e44c81c3d
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-10 17:12:40 +0900

    [Bronze II] Title: 블랙잭, Time: 148 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -5,8 +5,8 @@ const input = fs.readFileSync(file).toString().trim().split('\n');
 const [[n, m], cards] = input.map((s) => s.split(' ').map(Number));
 let max = 0;
 
-for (let i = 0; i < n; i++) {
-  for (let j = i + 1; j < n; j++) {
+for (let i = 0; i < n - 2; i++) {
+  for (let j = i + 1; j < n - 1; j++) {
     for (let k = j + 1; k < n; k++) {
       const sum = cards[i] + cards[j] + cards[k];
       if (sum <= m && sum > max) {
