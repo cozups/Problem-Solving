2465050fd7b5b131066019ca01292bf63931bbea
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-13 11:24:26 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const inputs = fs.readFileSync(file).toString().trim().split('\n');
+const input = fs.readFileSync(file).toString().trim().split('\n');
 
-let [n, k] = inputs[0].split(' ').map(Number);
-let coins = inputs.slice(1).map(Number);
+let [[n, k], ...coin] = input.map((s, i) =>
+  i === 0 ? s.split(' ').map(Number) : +s
+);
 
-let ans = 0;
-for (let i = coins.length - 1; i >= 0; i--) {
-  ans += Math.floor(k / coins[i]);
-  k %= coins[i];
+let cnt = 0;
+
+for (let i = coin.length - 1; i >= 0; i--) {
+  cnt += Math.floor(k / coin[i]);
+  k = k % coin[i];
 }
 
-console.log(ans);
+console.log(cnt);
