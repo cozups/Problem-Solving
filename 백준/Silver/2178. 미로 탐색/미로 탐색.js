5d8411727a339c6cd046ad5dd9204a2a47b1f54d
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-24 13:05:53 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,38 +1,36 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const [meta, ...input] = fs.readFileSync(file).toString().trim().split('\n');
+const input = fs.readFileSync(file).toString().trim().split('\n');
 
-let [n, m] = meta.split(' ').map(Number);
-let map = [];
-for (let i = 0; i < n; i++) {
-  map.push(input[i].split('').map(Number));
-}
-
-let dir = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
-
+let [[n, m], ...map] = input.map((s, i) =>
+  i === 0 ? s.split(' ').map(Number) : s.split('').map(Number)
+);
 let visited = Array.from({ length: n }, () => new Array(m).fill(-1));
 
 function bfs() {
   let q = [[0, 0]];
   visited[0][0] = 1;
-  while (q.length > 0) {
-    let [x, y] = q.shift();
+
+  let dy = [-1, 1, 0, 0];
+  let dx = [0, 0, -1, 1];
+
+  while (q.length) {
+    let [y, x] = q.shift();
     for (let i = 0; i < 4; i++) {
-      let nextX = x + dir[i][0];
-      let nextY = y + dir[i][1];
-      if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m) continue;
-      if (map[nextX][nextY] === 1 && visited[nextX][nextY] === -1) {
-        q.push([nextX, nextY]);
-        visited[nextX][nextY] = visited[x][y] + 1;
+      let ny = y + dy[i];
+      let nx = x + dx[i];
+
+      if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+        continue;
+      }
+      if (map[ny][nx] === 1 && visited[ny][nx] === -1) {
+        visited[ny][nx] = visited[y][x] + 1;
+        q.push([ny, nx]);
       }
     }
   }
+
+  console.log(visited[n - 1][m - 1]);
 }
 
 bfs();
-console.log(visited[n - 1][m - 1]);
