403252a972252a0d5e1a20af173d0db6fb0b6dd9
Kim Miso <58796245+cozups@users.noreply.github.com>
2024-10-25 12:50:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 844 ms, Memory: 113948 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,33 +1,33 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const [meta, ...input] = fs.readFileSync(file).toString().trim().split('\n');
+const [nm, ...input] = fs.readFileSync(file).toString().trim().split('\n');
 
-let [n, m] = meta.split(' ').map(Number);
-let adj = Array.from({ length: n + 1 }, () => []);
+let [n, m] = nm.split(' ').map(Number);
+let list = Array.from({ length: n + 1 }, () => []);
 let visited = new Array(n + 1).fill(false);
 
 for (let i = 0; i < m; i++) {
-  let [u, v] = input[i].split(' ').map(Number);
-  adj[u].push(v);
-  adj[v].push(u);
+  let [a, b] = input[i].split(' ').map(Number);
+  list[a].push(b);
+  list[b].push(a);
 }
 
-function dfs(v) {
-  visited[v] = true;
+function dfs(x) {
+  visited[x] = true;
 
-  for (let u of adj[v]) {
-    if (!visited[u]) {
-      dfs(u);
+  for (let node of list[x]) {
+    if (!visited[node]) {
+      dfs(node);
     }
   }
 }
 
-let ans = 0;
+let cnt = 0;
 for (let i = 1; i <= n; i++) {
   if (!visited[i]) {
-    ans++;
+    cnt++;
     dfs(i);
   }
 }
 
-console.log(ans);
+console.log(cnt);
