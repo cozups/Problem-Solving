4edd7200b1066c9a8e424de077b22775479f42cb
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-10-30 19:19:43 +0900

    Time: 0 ms (100%), Space: 58.4 MB (34.7%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let p1 = 0;
+  let p2 = 0;
+
+  while (p2 < nums.length) {
+    if (nums[p2] !== nums[p1]) {
+      nums[++p1] = nums[p2];
+    }
+    p2++;
+  }
+
+  return p1 + 1;
+};
