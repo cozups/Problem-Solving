bc56c73285253da89059f82e8b3264c96d783d55
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-10-31 16:29:42 +0900

    Time: 13 ms (64.87%), Space: 60.9 MB (13.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -8,6 +8,8 @@ var isAnagram = function(s, t) {
     // 애너그램 확인 방법: 빈도수가 같은지 확인 -> 빈도수 객체
     // s 빈도수 체크 -> t 문자열의 문자가 등장 하지 않으면 false, 등장하면 -1
     // 객체가 모두 0이어야 애너그램
+    // 추가: 길이가 다르면 false
+    if(s.length !== t.length) return false;
 
     const freq = {};
     for(let char of s) {
@@ -19,5 +21,6 @@ var isAnagram = function(s, t) {
         if(freq[char]) freq[char]--;
     }
 
-    return Object.values(freq).filter(v => v !== 0).length === 0;
+    // 길이 체크를 하기 때문에 0 이상의 값이 남는지 확인할 필요가 없다.
+    return true;
 };
