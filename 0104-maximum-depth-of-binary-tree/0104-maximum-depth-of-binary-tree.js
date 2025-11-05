050b1b5ef1120c1c05c1ede7222713b9e647a99c
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-11-05 15:27:52 +0900

    Time: 0 ms (100%), Space: 57.4 MB (73.71%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -14,20 +14,6 @@ var maxDepth = function(root) {
     // 트리의 최대 깊이 구하기
     // DFS
     if(!root) return 0;
-    
-    let maxDepth = 0;
-    let depth = 1;
-    const stack = [[root, depth]];
-    while(stack.length > 0) {
-        let [current, currentDepth] = stack.pop();
-        maxDepth = Math.max(maxDepth, currentDepth);
-        if(current.left) {
-            stack.push([current.left, currentDepth + 1]);
-        }
-        if(current.right) {
-            stack.push([current.right, currentDepth + 1]);
-        }
-    }
 
-    return maxDepth;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
