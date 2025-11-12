861f937f87a32aef7e1793043d9200c59896d514
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-11-12 15:35:55 +0900

    Time: 7 ms (48.19%), Space: 59.5 MB (42.67%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,40 +3,48 @@
  * @return {number}
  */
 var maxAreaOfIsland = function (grid) {
-  // 섬의 최대 너비 구하기
-  let ans = 0;
-  const ROWS = grid.length;
-  const COLS = grid[0].length;
+    // 섬의 최대 너비 구하기
+    let ans = 0;
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
 
-  const dir = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
+    const dir = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1],
+    ];
 
-  function dfs(row, col, area) {
-    if (row < 0 || col < 0 || row >= ROWS || col >= COLS || grid[row][col] === 0) {
-      return 0;
+    function bfs(row, col) {
+        const q = new Queue();
+        q.push([row, col]);
+        grid[row][col] = 0;
+        let res = 1;
+
+        while (!q.isEmpty()) {
+            let [r, c] = q.pop();
+
+            for (let i = 0; i < 4; i++) {
+                const nextRow = r + dir[i][0];
+                const nextCol = c + dir[i][1];
+
+                if (0 <= nextRow && nextRow < ROWS && 0 <= nextCol && nextCol < COLS && grid[nextRow][nextCol]) {
+                    q.push([nextRow, nextCol]);
+                    grid[nextRow][nextCol] = 0;
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 
-    grid[row][col] = 0;
-    let res = 1;
-
-    for (let i = 0; i < 4; i++) {
-      res += dfs(row + dir[i][0], col + dir[i][1], area);
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c]) {
+                ans = Math.max(ans, bfs(r, c));
+            }
+        }
     }
 
-    return res;
-  }
-
-  for (let r = 0; r < ROWS; r++) {
-    for (let c = 0; c < COLS; c++) {
-      if (grid[r][c]) {
-        ans = Math.max(ans, dfs(r, c, 0));
-      }
-    }
-  }
-
-  return ans;
+    return ans;
 };
