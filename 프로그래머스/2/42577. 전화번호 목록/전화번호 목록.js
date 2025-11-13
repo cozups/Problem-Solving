bf706f4abfd0f8a428accd30f77f03014923145a
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-11-13 20:11:20 +0900

    [level 2] Title: 전화번호 목록, Time: 354.82 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,6 +1,6 @@
 function solution(phone_book) {
     const set = new Set();
-    phone_book = phone_book.sort((a,b) => a - b); // 글자수 정렬
+    phone_book = phone_book.sort(); // 글자수 정렬
     
     for(let phone of phone_book) {
         for(let i = 1; i <= phone.length; i++) {
