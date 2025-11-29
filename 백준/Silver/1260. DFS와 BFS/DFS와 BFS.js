6809eb18fa0ded50d315dc270132fe9ac1c9ff4c
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-11-29 13:37:57 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 16584 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -1,53 +1,67 @@
-const fs = require('fs');
-const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const [meta, ...input] = fs.readFileSync(file).toString().trim().split('\n');
+// 입력
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
 
-let [n, m, s] = meta.split(' ').map(Number);
-let edges = input.map((e) => e.split(' ').map(Number));
+// 입력값 가공
+input = input.map((line) => line.split(" ").map(Number));
 
-let adj = Array.from({ length: n + 1 }, () => []);
+// 풀이
+function solution() {
+  let [n, m, v] = input[0];
+  let adj = Array.from({ length: n + 1 }, () => []);
 
-for (let [s, e] of edges) {
-  adj[s].push(e);
-  adj[e].push(s);
-}
+  // 인접 리스트 생성
+  for (let i = 1; i <= m; i++) {
+    let [s, e] = input[i];
+    adj[s].push(e);
+    adj[e].push(s);
+  }
 
-for (let list of adj) {
-  list = list.sort((a, b) => a - b);
-}
+  // 인접 노드 정렬
+  for (let i = 1; i <= n; i++) {
+    adj[i].sort((a, b) => a - b);
+  }
 
-let dfsResult = [];
-let dfsVisited = new Array(n + 1).fill(false);
-function dfs(v) {
-  dfsVisited[v] = true;
-  dfsResult.push(v);
-  for (let n of adj[v]) {
-    if (!dfsVisited[n]) {
-      dfs(n);
+  // DFS
+  let visited = Array.from({ length: n + 1 }, () => false);
+  let answer = [];
+  function dfs(node) {
+    if (visited[node]) return;
+
+    visited[node] = true;
+    answer.push(node);
+    for (let next of adj[node]) {
+      dfs(next);
     }
   }
-}
 
-let bfsResult = [];
-let bfsVisited = new Array(n + 1).fill(false);
-function bfs(v) {
-  let q = [v];
-  bfsVisited[v] = true;
+  dfs(v);
+  console.log(answer.join(" "));
 
-  while (q.length > 0) {
-    let v = q.shift();
-    bfsResult.push(v);
-    for (let n of adj[v]) {
-      if (!bfsVisited[n]) {
-        bfsVisited[n] = true;
-        q.push(n);
+  // BFS
+  visited = Array.from({ length: n + 1 }, () => false);
+  answer = [];
+  function bfs(node) {
+    let queue = [node];
+
+    while (queue.length) {
+      let current = queue.shift();
+      if (visited[current]) continue;
+
+      visited[current] = true;
+      answer.push(current);
+
+      for (let next of adj[current]) {
+        if (!visited[next]) {
+          queue.push(next);
+        }
       }
     }
   }
+
+  bfs(v);
+  console.log(answer.join(" "));
 }
 
-dfs(s);
-console.log(dfsResult.join(' '));
-
-bfs(s);
-console.log(bfsResult.join(' '));
+solution();
