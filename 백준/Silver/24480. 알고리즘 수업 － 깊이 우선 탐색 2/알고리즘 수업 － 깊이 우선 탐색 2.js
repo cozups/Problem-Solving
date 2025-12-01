4413fe162935bf70e05f1eb7f3eba13fe2f7e14c
Kim Miso <58796245+cozups@users.noreply.github.com>
2025-12-01 18:23:54 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 692 ms, Memory: 107132 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,38 @@
+// 입력
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+// 입력값 가공
+let [[n, m, r], ...rest] = input.map((line) => line.split(" ").map(Number));
+
+// 풀이
+function solution() {
+  let adj = Array.from({ length: n + 1 }, () => []);
+
+  for (let i = 0; i < m; i++) {
+    let [u, v] = rest[i];
+    adj[u].push(v);
+    adj[v].push(u);
+  }
+
+  adj = adj.map((list) => list.sort((a, b) => b - a));
+
+  let visited = Array.from({ length: n + 1 }, () => 0);
+  let count = 1;
+  function dfs(node) {
+    if (visited[node]) return;
+
+    visited[node] = count++;
+
+    for (let next of adj[node]) {
+      dfs(next);
+    }
+  }
+
+  dfs(r);
+
+  console.log(visited.slice(1).join("\n"));
+}
+
+solution();
